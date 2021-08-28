08a72fa8d96b0f7965d1db9263dec0438f4e3e6c
Archana28S <89697550+Archana28S@users.noreply.github.com>
2021-08-28 20:50:44 +0530

    Add files via upload

---
+++ Problem#0.py
@@ -0,0 +1,30 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type " help", "copyright", "credits" or "license()" for more information.
+
+# Problem Set 0
+# Name: Archana Sharma
+# Time Spent: 00:40
+1)
+>>> input("Enter your date of birth")
+Enter your date of birth 20/08/2001
+' 20/08/2001'
+>>> input("Enter your last name")
+Enter your last name Sharma
+' Sharma'
+>>> print("Sharma, 20/08/2001")
+Sharma, 20/08/2001
+>>> 
+
+2)
+>>> import math
+>>> math.pow(23.0,5)
+6436343.0
+
+3)
+import math
+>>> a=34
+>>> b=68
+>>> c= -510
+>>> x=(-68+math.sqrt(68*68-4*34*-510))/(2*34)
+>>> print(x)
+3.0
